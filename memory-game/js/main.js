b1e70cf2bdbce94b7f6057f75c89465c001e1d66
杨晨珑 <yangchenlong@yangchenlongdeMacBook-Pro.local>
2020-04-25 17:42:42 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,23 +3,25 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardID){
+
+	console.log("User flipped " + cards[cardID]);
+
+	cardsInPlay.push(cards[cardID]);
+
+	if (cardsInPlay.length>1){
+		checkForMatch();
+	}
+	
+}
+
+flipCard(0);
+flipCard(2);
